db8140522eaf76b3742f4396e435733499efc680
admin <kandalgaonkarritesh76@gmail.com>
2024-11-04 14:16:32 +0530

    initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import {createRoot} from "react-dom/client";
+import React from "react";
+function App()
+{
+    return <h1>ritesh</h1>;
+}
+
+
+const root=createRoot(document.getElementById("app"));
+
+root.render(<App />);
